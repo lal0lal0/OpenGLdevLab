a3aa48e98fe220bfcbf5786d98d5c4fe0628f14f
MiMiau <eduardojcr@gmail.com>
2018-03-07 09:38:01 -0600

    Siguiendo el tutorial
    hasta aqui ya leo el codigo glsl en archivos de texto con una utileria en java, ya compilo en teoria y encadeno uno los shaders. todo segun el tutorial.

--- SkinnerApp/app/src/main/java/vector3/mx/skinnerapp/OpenGLRendererPrincipal.java
+++ SkinnerApp/app/src/main/java/vector3/mx/skinnerapp/OpenGLRendererPrincipal.java
@@ -20,6 +20,7 @@ public class OpenGLRendererPrincipal implements Renderer {
     private static final int POSITION_COMPONENT_COUNT = 2;
     private static final int BYTES_PER_FLOAT = 4;
     private final FloatBuffer vertexData;
+    private int program = 0;
 
     //Constructor
     public OpenGLRendererPrincipal(Context context){
@@ -46,10 +47,13 @@ public class OpenGLRendererPrincipal implements Renderer {
                 .asFloatBuffer();
         vertexData.put(tableVerticesWithTriangles);
     }
+
     @Override
     public void onSurfaceCreated(GL10 glUnused, EGLConfig config) {
-
         glClearColor(1f, 0f, 0f, 1f);
+        int vertexShader = ShaderHelper.compileVertexShader(TextResourceReader.readTextFileFromResource(context, R.raw.simple_vertex_shader));
+        int fragmentShader = ShaderHelper.compileFragmentShader(TextResourceReader.readTextFileFromResource(context, R.raw.fragment_shader));
+        program = ShaderHelper.linkProgram(vertexShader, fragmentShader);
     }
 
     @Override
