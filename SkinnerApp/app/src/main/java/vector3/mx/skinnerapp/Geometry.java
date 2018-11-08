fb9c433d6fb0f3571006f9c3662e8ed90c051e2c
MiMiau <lalo.gdl@live.com>
2018-11-08 08:22:40 -0600

    Agregando movimiento y deteccion de colisiones
    Anteriormente el proyecto quedo estable con los shaders siendo capaz de crear objetos con la tecnica de triangle strip y triangle fan, tmbn tiene la capacidad de obtener la configuracion de fragment shader integrando el codigo r g b en mismo arreglo vertexArray, la configuracion para la representacion adecuada de los vertices en las coordenadas de la pantalla es funcional , y estamos desarrollando el codigo para darle movimiento a los mallets y a la pelota para detectar coliciones con velocidad y todo.

--- SkinnerApp/app/src/main/java/vector3/mx/skinnerapp/Geometry.java
+++ SkinnerApp/app/src/main/java/vector3/mx/skinnerapp/Geometry.java
@@ -1,5 +1,13 @@
 package vector3.mx.skinnerapp;
 
+
+
+import android.util.FloatMath;
+
+import java.util.Vector;
+
+import static android.util.FloatMath.*;
+
 /**
  * Created by miMiau on 10/06/2018.
  */
@@ -26,6 +34,14 @@ public class Geometry {
         //Mover en vertical its a helper function
         public Point translateX(float distance) {
             return new Point(x + distance, y , z);
+        }
+
+        public Point translate(Vector vector){
+            return new Point(
+                    x + vector.x,
+                    y + vector.y,
+                    z + vector.z
+            );
         }
     }
 
@@ -92,6 +108,109 @@ public class Geometry {
         }
     }
 
+
+    public static class Ray{
+        public final Point point;
+        public final Vector vector;
+
+        public Ray(Point point, Vector vector){
+            this.point = point;
+            this.vector = vector;
+        }
+    }
+
+    public static class Vector{
+        public final float x, y ,z;
+
+        public Vector(float x, float y, float z){
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public float length(){
+            return (float)Math.sqrt(x * x + y * y + z * z);
+        }
+
+        public Vector crossProduct(Vector other){
+            return new Vector(
+                    (y * other.z) - (z * other.y),
+                    (z * other.x) - (x * other.z),
+                    (x * other.y) - (y * other.x)
+            );
+        }
+
+        public float dotProduct(Vector other){
+            return x * other.x + y * other.y + z * other.z;
+        }
+
+        public Vector scale(float f){
+            return new Vector( x * f, y * f , z * f);
+        }
+    }
+
+    public static Vector vectorBetween(Point from, Point to){
+        return new Vector(
+                to.x - from.x,
+                to.y - from.y,
+                to.z - from.z
+        );
+    }
+
+    public static class Sphere{
+        public final Point center;
+        public final float radius;
+
+        public Sphere(Point center, float radius){
+            this.center = center;
+            this.radius = radius;
+        }
+    }
+
+    public static boolean intersects(Sphere sphere, Ray ray){
+        return distanceBetween(sphere.center, ray) < sphere.radius;
+    }
+
+    public static Point intersectionPoint(Ray ray, Plane plane){
+        Vector rayToPlaneVector =
+                vectorBetween(ray.point, plane.point);
+        float scaleFactor =
+                rayToPlaneVector.dotProduct(plane.normal) / ray.vector.dotProduct(plane.normal);
+        Point intersectionPoint = ray.point.translate(ray.vector.scale(scaleFactor));
+        return intersectionPoint;
+    }
+
+    public static  float distanceBetween(Point point, Ray ray){
+        Vector p1ToPoint = vectorBetween(ray.point, point);
+        Vector p2ToPoint = vectorBetween(ray.point.translate(ray.vector), point);
+
+        /*
+        The length of the cross product gives the area of an imaginary
+        parallelogram having the two vectors as sides. A parallelogram
+        can be thought of as consisting of two triangles, so this is the same
+        as twice the area of the  triangle defined by the two vectors.
+         */
+        float areaOfTriangleTimesTwo = p1ToPoint.crossProduct(p2ToPoint).length();
+        float lengthOfBase = ray.vector.length();
+
+        /*
+        The area of a triangle is also equal to (base * height) / 2.In
+        other words , the height is equal to ( area * 2) / base. The height
+        of this triangle is the distance from the point to the ray.
+         */
+        float distanceFromPointToRay = areaOfTriangleTimesTwo / lengthOfBase;
+        return distanceFromPointToRay;
+    }
+
+    public static class Plane{
+        public final Point point;
+        public final Vector normal;
+
+        public Plane(Point point, Vector normal){
+            this.point = point;
+            this.normal = normal;
+        }
+    }
 
 
 }
--- SkinnerApp/app/src/main/java/vector3/mx/skinnerapp/OpenGLRendererPrincipal.java
+++ SkinnerApp/app/src/main/java/vector3/mx/skinnerapp/OpenGLRendererPrincipal.java
@@ -9,15 +9,19 @@ import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 import android.content.Context;
 import android.opengl.GLSurfaceView.Renderer;
-
+import android.util.Log;
+import vector3.mx.skinnerapp.Geometry.*;
 
 
 public class OpenGLRendererPrincipal implements Renderer {
+
     private final Context context;
+    private static final String TAG = "MainRenderer";
 
     private final float[] viewMatrix = new float[16];
     private final float[] viewProjectionMatrix = new float[16];
     private final float[] modelViewProjectionMatrix = new float[16];
+    private final float[] invertedViewProjectionMatrix = new float[16];
 
     private final float[] projectionMatrix = new float[16];
     private final float[] modelMatrix = new float[16];
@@ -30,6 +34,10 @@ public class OpenGLRendererPrincipal implements Renderer {
     private ColorShaderProgram colorProgram;
     private int texture;
 
+    private boolean malletPressed = false;
+    private Point redMalletPosition;
+
+
     //Constructor
     public OpenGLRendererPrincipal(Context context) {
         this.context = context;
@@ -40,6 +48,7 @@ public class OpenGLRendererPrincipal implements Renderer {
         glClearColor(0f, 0f, 0f, 0f);
         puck = new Puck(0.035f, 0.015f, 30);
         mallet = new Mallet(0.060f, 0.090f, 30);
+        redMalletPosition = new Point(0f, mallet.height / 2f, 0.4f);
         tabla = new Table();
         textureProgram = new TextureShaderProgram(context);
         colorProgram = new ColorShaderProgram(context);
@@ -60,6 +69,7 @@ public class OpenGLRendererPrincipal implements Renderer {
     public void onDrawFrame(GL10 gl10) {
         GLES20.glClear(GL_COLOR_BUFFER_BIT);
         multiplyMM(viewProjectionMatrix, 0, projectionMatrix, 0, viewMatrix, 0);
+        invertM(invertedViewProjectionMatrix, 0, viewProjectionMatrix,0);
 
         positionTableInScene();
         textureProgram.useProgram();
@@ -89,7 +99,6 @@ public class OpenGLRendererPrincipal implements Renderer {
                 0, modelMatrix, 0);
     }
 
-
     private void positionObjectInScene(float x, float y, float z){
         setIdentityM(modelMatrix,0);
         translateM(modelMatrix, 0, x, y , z);
@@ -104,4 +113,64 @@ public class OpenGLRendererPrincipal implements Renderer {
                 0, modelMatrix, 0);
     }
 
+    public void handleTouchPress(float normalizedX, float normalizedY){
+        Log.i(TAG, "movimiento detectado TOUCH en X : " + normalizedX );
+        Ray ray = convertNormalized2DPointToRay(normalizedX, normalizedY);
+        // Now test if this ray intersects with the mallet by creating
+        //   a bounding sphere that wraps the mallet.
+        Sphere malletBoundingSphere = new Sphere(new Point(
+                redMalletPosition.x,
+                redMalletPosition.y,
+                redMalletPosition.z),
+                mallet.height / 2f);
+        //if the ray intersects  ( if the user  touched  a part  of the screen that
+        //intersects the mallets  bounding sphere ) ,then set mallet pressed  = true
+        malletPressed = Geometry.intersects(malletBoundingSphere, ray);
+    }
+
+    private Ray convertNormalized2DPointToRay(float normalizedX, float normalizedY){
+        //we'll convert these normalized device coordinates into world space
+        // coordinates. well pick a point on the near far planes , and draw
+        // a line between them. To do this transform , we need to first multiply
+        // by the inverse matrix , and then we need to undo the perspective divide.
+        final float[] nearPointNdc = {normalizedX, normalizedY, -1, 1};
+        final float[] farPointNdc = {normalizedX, normalizedY, 1, 1};
+
+        final float[] nearPointWorld = new float[4];
+        final float[] farPointWorld = new float[4];
+
+        multiplyMV(
+                nearPointWorld, 0, invertedViewProjectionMatrix, 0 , nearPointNdc, 0);
+        multiplyMV(farPointWorld, 0, invertedViewProjectionMatrix, 0, farPointNdc, 0);
+
+        divideByW(nearPointWorld);
+        divideByW(farPointWorld);
+
+        Point nearPointRay =
+                new Point(nearPointWorld[0], nearPointWorld[1], nearPointWorld[2]);
+        Point farPointRay =
+                new Point(farPointWorld[0], farPointWorld[1], farPointWorld[2]);
+
+        return new Ray(nearPointRay, Geometry.vectorBetween(nearPointRay, farPointRay));
+    }
+    private void divideByW(float[] vector){
+        vector[0] /= vector[3];
+        vector[1] /= vector[3];
+        vector[2] /= vector[3];
+    }
+
+    public void handleTouchDrag(float normalizedX, float normalizedY){
+        Log.i(TAG, "movimiento detectado DRAG en X : " + normalizedX );
+        if(malletPressed){
+            Ray ray = convertNormalized2DPointToRay(normalizedX, normalizedY);
+            //Define a plane representing our air hockey table.
+            Plane plane = new Plane(new Point(0, 0, 0), new Vector(0, 1, 0));
+            /*
+            find out where the touched point intersects the plane
+            representing our table. well move the mallet along this plane.
+             */
+            Point touchedPoint = Geometry.intersectionPoint(ray, plane);
+            redMalletPosition = new Point(touchedPoint.x, mallet.height / 2f, touchedPoint.z);
+        }
+    }
 }
